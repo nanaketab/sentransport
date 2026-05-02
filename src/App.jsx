874a7e1e4bc0e3613d3a9603d7b553e1172f6296
nana ketab <nanaketab@esp.sn>
2026-05-02 16:00:23 +0000

    Exercices : Footer, date et Statistiques

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import './App.css'
 import Header from './Header'
+import Footer from './Footer'
+import Statistique from './Statistique'
+import Statistique2 from './Statistique2'
+import Statistique3 from './Statistique3'
 
 function App() {
   return (
@@ -8,7 +12,13 @@ function App() {
       <main className="contenu">
         <p>Bienvenue ! Cette application vous aide à trouver
         votre ligne de bus à Dakar.</p>
+        <div className="stats">
+          <Statistique />
+          <Statistique2 />
+          <Statistique3 />
+        </div>
       </main>
+      <Footer />
     </div>
   )
 }
